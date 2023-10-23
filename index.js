f793f3d5d7d887574c8f53ba67b8d889d38ee6d9
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-10-23 00:01:39 -0400

    Added steps to confirm a palindrome and tests passed

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word === word.split("").reverse().join("")){return true}
+  return false;
 }
 
 /* 
   Add your pseudocode here
+  If the word is equal to the reverse organization, it is a palindrome. 
 */
 
 /*
   Add written explanation of your solution here
+  1. split the word 
+  2. use the Array.prototype.reverse() method to reverse the elements order
+  3. join the elements with .join("")
+  4. compare the word with the reverse and if equal, return true
+  5. otherwise, return false
 */
 
 // You can run `node index.js` to view these console logs
